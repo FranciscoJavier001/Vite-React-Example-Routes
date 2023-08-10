3c2630687e7c744dfccdd1ab150a1731a338905a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-08-10 15:34:39 -0600

    Clear Base Url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://FranciscoJavier001.github.io/Vite-React-Example-Routes/",
 });
